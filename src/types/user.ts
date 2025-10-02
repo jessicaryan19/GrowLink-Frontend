978567b6d34ca13be9f327d4f38a45f7900ba30f
Ivan <alexander.gumilang-guest@moladin.com>
2025-10-03 00:23:02 +0700

    feat: enhance project and student management features
    - Added axios dependency and implemented user registration functionality.
    - Updated ProjectCard and StudentCard components to accept project and student data as props.
    - Implemented project and student fetching logic in Projects and Students pages.
    - Created new types for Project and Student, along with their respective list requests.
    - Refactored build script in package.json for better TypeScript configuration.
    - Added Pagination component for future use.

--- src/types/user.ts
+++ src/types/user.ts
@@ -7,3 +7,29 @@ export type RegisterUserRequest = {
   company_name?: string
   university?: string
 }
+
+export type Student = {
+  uuid: string
+  user_uuid: string
+  email: string
+  name: string
+  university: string
+  skills: string[] | null
+  created_at: string
+}
+
+export type StudentListRequest = {
+  search?: string
+  location?: string
+  skills?: string
+  page: number
+  limit: number
+}
+
+export type StudentListResponse = {
+  students: Student[]
+  total_count: number
+  page: number
+  limit: number
+  total_pages: number
+}
